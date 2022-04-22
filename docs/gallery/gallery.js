419b6b6d848fd066e9ef9da6931ad0f30d516d9b
John Cheung <61215358+Real-John-Cheung@users.noreply.github.com>
2022-04-22 21:53:40 +0800

    finish website

---
+++ docs/gallery/gallery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick-container').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+  });
